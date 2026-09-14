ff1c4fb8faf1887d02665cec22d6434d1db60a4b
sarina-loli <sarina123getu@gmail.com>
2026-09-14 03:20:47 -0700

    3

--- back/config/settings.py
+++ back/config/settings.py
@@ -129,7 +129,8 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:5173",
-        "http://127.0.0.1:5173",]
+        "http://127.0.0.1:5173",
+        "https://portofolio-1-0siw.onrender.com"]
    
 
 
